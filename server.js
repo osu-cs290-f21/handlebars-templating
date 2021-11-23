b8e4d10d644932c01cdad325539c7f82a4f6ced5
Rob Hess <hessro@oregonstate.edu>
2021-11-23 14:48:46 -0800

    Add code from 11/23 lecture (1st half).

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var exphbs = require('express-handlebars')
 
 var port = process.env.PORT || 8000;
 var app = express();
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: null }))
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'));
 
@@ -20,9 +24,12 @@ var availablePeople = [
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
   if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
+    // res.status(200).sendFile(
+    //   __dirname + '/public/people/' + person + '.html'
+    // );
+    res.status(200).render('photoPage', {
+      name: 'Rey (as a kitty)'
+    })
   } else {
     next();
   }
