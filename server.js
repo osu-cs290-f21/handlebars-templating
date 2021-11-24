47456dbdc65f95e82364b334795b1ed23c70118c
Rob Hess <hessro@oregonstate.edu>
2021-11-23 16:32:37 -0800

    Add code from 11/23 lecture.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var exphbs = require('express-handlebars')
 
+var peopleData = require('./peopleData.json')
+console.log("== peopleData:", peopleData)
+
 var port = process.env.PORT || 8000;
 var app = express();
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: null }))
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'));
@@ -13,22 +16,20 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/people.html');
 });
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-];
-
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
-  if (availablePeople.indexOf(person) >= 0) {
+  var personData = peopleData[person]
+  console.log("  -- personData:", personData)
+
+  if (personData) {
     // res.status(200).sendFile(
     //   __dirname + '/public/people/' + person + '.html'
     // );
+
     res.status(200).render('photoPage', {
-      name: 'Rey (as a kitty)'
+      name: personData.name,
+      photos: personData.photos,
+      showTheHeader: false
     })
   } else {
     next();
@@ -36,7 +37,10 @@ app.get('/people/:person', function (req, res, next) {
 });
 
 app.get("*", function (req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
+  // res.status(404).sendFile(__dirname + '/public/404.html');
+  res.status(404).render('404', {
+    path: req.url
+  })
 });
 
 app.listen(port, function (err) {
